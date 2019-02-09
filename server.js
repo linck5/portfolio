7a41d5402b253442a860cdd0e5a207065528460a
Unknown <linck_192@hotmail.com>
2019-02-09 17:24:18 -0200

    node wrapper

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const path = require ('path')
+
+const app = express();
+
+app.use(express.static(__dirname + '/dist/portfolio'))
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/portfolio/index.html'))
+})
+
+app.listen(process.env.PORT || 8080)
