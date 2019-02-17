e0536c8428d90f6de39d93b083aad99585be8802
Unknown <linck_192@hotmail.com>
2019-02-17 19:17:38 -0300

    improved text-animator

--- src/app/globalServices/textAnimator/text-animator.service.ts
+++ src/app/globalServices/textAnimator/text-animator.service.ts
@@ -1,5 +1,18 @@
 import { Injectable } from '@angular/core';
-import * as possibleDic from './possibleDic.json'
+import possibleDic from './possibleDic'
+
+interface AnimationOptions{
+  charsPerSection?: number
+  sectionGenerationDelay?: number
+  numberGenerationSlowness?: number
+  chanceToAddNewGeneration?: number
+  newCharDelayScale?: number
+  newCharDelayMin?: number
+  newCharDelayMax?: number
+  possibleRepeatAmount?: number
+  charPossibilitiesSize?: number
+  ignoreSpace?: boolean
+}
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +23,52 @@ export class TextAnimatorService {
   domElement:any
   targetText:string
   mainLoop:any
-  charsPerSolve = 10
-  solvingSections = []
-  solvingTargets = []
-  possibles = []
+  solvingSections:string[]
+  solvingTargets:string[]
+  possibles:any[]
+  updateCharTimeouts:object = {}
+  timeoutCount:number
+  opt:AnimationOptions
 
 
   constructor() { }
 
-  animateText(targetText: string, domElement, options: object) {
+
+  animateText(targetText: string, domElement, options: object = {}) {
+
+    Object.keys(this.updateCharTimeouts).forEach((key)=>{
+      clearTimeout(this.updateCharTimeouts[key])
+    })
+    if(this.mainLoop){
+        clearInterval(this.mainLoop)
+    }
+
+    this.updateCharTimeouts = {}
+    this.timeoutCount = 0
+    this.solvingSections = []
+    this.solvingTargets = []
+    this.possibles = []
+
+
+    this.opt = {
+      charsPerSection: 3,
+      sectionGenerationDelay: 20,
+      numberGenerationSlowness: 2,
+      chanceToAddNewGeneration: 80,
+      newCharDelayScale: 3000,
+      newCharDelayMin: 50,
+      newCharDelayMax: 750,
+      possibleRepeatAmount: 2,
+      charPossibilitiesSize: 8,
+      ignoreSpace: true
+    }
+    Object.assign(this.opt, options)
 
     this.defaultPossible = this.uniqueChars(targetText)
     this.domElement = domElement
     this.targetText = targetText
-    this.mainLoop = setInterval(this.generate.bind(this), 50)
-    return possibleDic["a"]
+    this.mainLoop = setInterval(
+      this.generate.bind(this), this.opt.sectionGenerationDelay)
   }
 
   private uniqueChars(str:string){
@@ -57,11 +101,26 @@ export class TextAnimatorService {
     let plainTarget =
         target.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase()
 
+    if(this.opt.ignoreSpace && plainTarget == " "){
+      return " "
+    }
     if(possibleDic[plainTarget] != undefined){
-      return possibleDic[plainTarget] + target
+
+      let possible = possibleDic[plainTarget].repeat(this.opt.possibleRepeatAmount)
+      let charsToAdd = this.opt.charPossibilitiesSize - possible.length
+
+      for (let _ = 0; _ < charsToAdd; _++) {
+          let randomIndex =
+            Math.floor(Math.random() * this.defaultPossible.length)
+          possible += this.defaultPossible[randomIndex]
+      }
+
+      return possible + target
     }
     else if(!isNaN(plainTarget)){
-      let possible = "0123456789".replace(plainTarget, '').repeat(10)
+      let possible = "0123456789".replace(plainTarget, '')
+        .repeat(this.opt.numberGenerationSlowness)
+
       return possible + target
     }
     else{
@@ -71,28 +130,17 @@ export class TextAnimatorService {
 
   private attemptChar(possiblesIndex){
     let char = this.randomChar(this.possibles[possiblesIndex])
-    this.possibles[possiblesIndex] =
-      this.replaceAt(this.possibles[possiblesIndex], this.possibles[possiblesIndex].indexOf(char), "")
+    this.possibles[possiblesIndex] = this.replaceAt(
+      this.possibles[possiblesIndex],
+      this.possibles[possiblesIndex].indexOf(char),
+      "")
 
     return char
   }
 
   private updateDom(){
     let rawText = this.solvingSections.join('')
-    let taggedText = ""
-    for(let i = 0; i < this.possibles.length; i++){
-      if(this.possibles[i] == null){
-        //taggedText += '<span>'+rawText[i]+'</span>'
-        taggedText += rawText[i]
-      }
-      else{
-        let progress = this.possibles[i].length / this.defaultPossible.length
-
-        taggedText += rawText[i]
-      }
-
-    }
-    this.domElement.textContent = taggedText
+    this.domElement.textContent = rawText
   }
 
   private generate() {
@@ -101,7 +149,7 @@ export class TextAnimatorService {
     let solvedCharsCount = 0
 
     for(let p of this.possibles){
-      if(p == null) solvedCharsCount++
+      if(p.done) solvedCharsCount++
     }
 
     let unsolvedChars = fullText.length - solvedCharsCount
@@ -113,60 +161,76 @@ export class TextAnimatorService {
       return
     }
 
-    if(Math.floor(Math.random() * Math.ceil(unsolvedChars / 20)) == 0 &&
-      this.targetText.length != fullText.length){
+    if( Math.floor(Math.random() *
+        Math.ceil(unsolvedChars / this.opt.chanceToAddNewGeneration)) == 0 &&
+        this.targetText.length != fullText.length){
       this.solvingSections.push("")
     }
 
 
     for(let j = 0; j < this.solvingSections.length; j++){
 
-      if(this.solvingSections[j] == this.solvingTargets[j]){
+      if(this.solvingSections[j] == ""){
 
-      }
-      else if(this.solvingSections[j] == ""){
-
-        let charsToSolve = this.charsPerSolve
-        let remainingGlobalCharsToSolve = this.targetText.length - this.solvingSections.join('').length;
+        let charsToSolve = this.opt.charsPerSection
+        let remainingGlobalCharsToSolve =
+          this.targetText.length - this.solvingSections.join('').length;
 
         if(remainingGlobalCharsToSolve < charsToSolve){
           charsToSolve = remainingGlobalCharsToSolve
         }
 
-        this.solvingTargets[j] = this.targetText.substring(fullText.length, fullText.length + charsToSolve)
+              this.solvingTargets[j] = this.targetText.substring(
+          fullText.length, fullText.length + charsToSolve)
         this.solvingSections[j] = this.addRandomChars("", charsToSolve)
 
         for(let i = 0; i < charsToSolve; i++){
 
           this.possibles.push(
-            this.determinePossibles(this.solvingSections[j][i], this.solvingTargets[j][i]))
+            this.determinePossibles(
+              this.solvingSections[j][i],
+              this.solvingTargets[j][i]))
           let possiblesIndex = this.possibles.length - 1
 
-          let updateChar = function(j, i, possiblesIndex){
-            if(this.solvingSections[j][i] != this.solvingTargets[j][i]){
-
-              this.solvingSections[j] =
-                this.replaceAt(this.solvingSections[j], i, this.attemptChar(possiblesIndex))
-
-              this.updateDom()
-
-              let delay = 3000/(-1+this.possibles[possiblesIndex].length)
-              let minDelay = 50
-              let maxDelay = 750
-
-              delay = Math.min(Math.max(delay, minDelay), maxDelay);
-
-              setTimeout(updateChar.bind(this), delay, j, i, possiblesIndex)
-            }
-            else{
-              this.possibles[possiblesIndex] = null
-            }
-          }
-
-          setTimeout(updateChar.bind(this), 5, j, i, possiblesIndex)
+          this.updateCharTimeouts[this.timeoutCount.toString()] =
+            setTimeout(this.updateChar.bind(this), 5, j, i, possiblesIndex, this.timeoutCount++)
         }
       }
     }
+  }
+
+  updateChar(j, i, possiblesIndex, timeoutIndex){
+    if(this.solvingSections[j][i] != this.solvingTargets[j][i]){
+
+      this.solvingSections[j] = this.replaceAt(
+        this.solvingSections[j],
+        i,
+        this.attemptChar(possiblesIndex))
+
+      this.updateDom()
+
+      let delay =
+        this.opt.newCharDelayScale /
+        (-1+this.possibles[possiblesIndex].length)
+
+      delay = Math.min(Math.max(
+        delay, this.opt.newCharDelayMin), this.opt.newCharDelayMax);
+
+      this.clearCharTimeout(timeoutIndex)
+
+      this.updateCharTimeouts[this.timeoutCount.toString()] =
+        setTimeout(this.updateChar.bind(this), delay, j, i, possiblesIndex, this.timeoutCount++)
+    }
+    else{
+      this.clearCharTimeout(timeoutIndex)
+
+      this.possibles[possiblesIndex] = {done:true, params:[j,i,possiblesIndex]}
+    }
+  }
+
+  clearCharTimeout(timeoutIndex){
+    clearTimeout(this.updateCharTimeouts[timeoutIndex])
+    delete this.updateCharTimeouts[timeoutIndex]
   }
 
 }
