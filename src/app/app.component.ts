b701176e289959946fa750612f7658c8155496b8
Unknown <linck_192@hotmail.com>
2019-02-19 14:32:56 -0300

    contact content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core'
 import AOS from 'aos'
+import * as $ from 'jquery';
+declare var google:any
 
 @Component({
   selector: 'app-root',
@@ -24,5 +26,28 @@ export class AppComponent {
         easing: 'ease-out-sine',
         once: true
       })
+
+      $(function () {
+          function initMap() {
+            var uluru = {lat: -26.2717061, lng: -47.9335963};
+            var map = new google.maps.Map(document.getElementById('map'), {
+              zoom: 7,
+              center: uluru,
+              scrollwheel: false,
+              zoomControlOptions: {
+                  position: google.maps.ControlPosition.LEFT_BOTTOM
+              },
+              streetViewControl: false,
+              mapTypeControl: false
+            });
+            var marker = new google.maps.Marker({
+              position: {lat: -26.489922, lng: -49.077456},
+              map: map
+            });
+
+          }
+
+          google.maps.event.addDomListener(window, 'load', initMap);
+      });
   }
 }
