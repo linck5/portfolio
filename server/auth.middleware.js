538ca1a62c86aff3157ae27fa6f48db26c22cfe3
Unknown <linck_192@hotmail.com>
2019-02-28 18:00:59 -0300

    fix auth throwing error when req sent without auth

--- server/auth.middleware.js
+++ server/auth.middleware.js
@@ -2,17 +2,18 @@ module.exports = function (req, res, next){
   if(!req.headers.authorization){
     res.status(401).send("Authorization Required");
   }
-
-  let [username, password] =
-    new Buffer.from(req.headers.authorization.split(" ").pop(), "base64")
-    .toString("ascii").split(":")
-
-  if(username !== process.env.API_USERNAME ||
-    password !== process.env.API_PASSWORD
-  ){
-    res.status(403).send("Access Denied (incorrect credentials)");
-  }
   else{
-    next()
+    let [username, password] =
+      new Buffer.from(req.headers.authorization.split(" ").pop(), "base64")
+      .toString("ascii").split(":")
+
+    if(username !== process.env.API_USERNAME ||
+      password !== process.env.API_PASSWORD
+    ){
+      res.status(403).send("Access Denied (incorrect credentials)");
+    }
+    else{
+      next()
+    }
   }
 }
