8191112e32ee2871c6b5aec3e22bbe837d8c4fee
Unknown <linck_192@hotmail.com>
2019-02-28 21:16:51 -0300

    moved google maps init to contact component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core'
 import AOS from 'aos'
-import * as $ from 'jquery';
-declare var google:any
+
 
 @Component({
   selector: 'app-root',
@@ -27,27 +26,6 @@ export class AppComponent {
         once: true
       })
 
-      $(function () {
-          function initMap() {
-            var uluru = {lat: -26.2717061, lng: -47.9335963};
-            var map = new google.maps.Map(document.getElementById('map'), {
-              zoom: 7,
-              center: uluru,
-              scrollwheel: false,
-              zoomControlOptions: {
-                  position: google.maps.ControlPosition.LEFT_BOTTOM
-              },
-              streetViewControl: false,
-              mapTypeControl: false
-            });
-            var marker = new google.maps.Marker({
-              position: {lat: -26.489922, lng: -49.077456},
-              map: map
-            });
 
-          }
-
-          google.maps.event.addDomListener(window, 'load', initMap);
-      });
   }
 }
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+declare var google:any
 
 @Component({
   selector: 'app-contact',
@@ -7,7 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    $(function () {
+        function initMap() {
+          var uluru = {lat: -26.2717061, lng: -47.9335963};
+          var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 7,
+            center: uluru,
+            scrollwheel: false,
+            zoomControlOptions: {
+                position: google.maps.ControlPosition.LEFT_BOTTOM
+            },
+            streetViewControl: false,
+            mapTypeControl: false
+          });
+          var marker = new google.maps.Marker({
+            position: {lat: -26.489922, lng: -49.077456},
+            map: map
+          });
+
+        }
+
+        google.maps.event.addDomListener(window, 'load', initMap);
+    });
+  }
 
   ngOnInit() {
   }
