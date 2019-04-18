1012b189e45ed07de79d5fb59a6e733a72b81e65
Unknown <linck_192@hotmail.com>
2019-04-18 14:34:55 -0300

    front-end mywork displaying data from the server

--- src/app/mywork/mywork.component.ts
+++ src/app/mywork/mywork.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, NgModule } from '@angular/core';
 import { TranslateService, LangChangeEvent } from '@ngx-translate/core'
 import { MyworkPipe } from './mywork.pipe'
+import { HttpService } from 'src/app/globalServices/http-service/http.service'
 
 
 interface Work {
@@ -22,7 +23,7 @@ export class MyworkComponent implements OnInit {
 
 
 
-
+  worksData:any;
 
 
   works:Work[] = [
@@ -73,17 +74,56 @@ export class MyworkComponent implements OnInit {
 
   private onLangChange(event: LangChangeEvent) {
 
-    console.log('Lang changed');
-    console.log(event)
+    if(!this.isFirstOnLangChangeEvent){
+      this.updateWorks(event.lang)
+    }
+
     this.isFirstOnLangChangeEvent = false;
   }
 
-  constructor(public translate: TranslateService) { }
+  private updateWorks(lang){
+    console.log(lang)
 
-  ngOnInit() {
+    this.works = []
+
+    const i18nProps = ["title", "imageUrl", "description", "type", "workUrl", "sourceCodeUrl"]
+    const otherProps = ["type", "date", ]
+
+    for (let work of this.worksData) {
+        let newWork:any = {}
+        for (let prop of i18nProps) {
+          if(lang == "en"){
+            if(work[prop]){
+              console.log("here")
+              newWork[prop] = work[prop]
+            }
+          }
+          else{
+            if(work.i18n && work.i18n[lang] && work.i18n[lang][prop]){
+              newWork[prop] = work.i18n[lang][prop]
+            }
+            else{
+              newWork[prop] = work[prop]
+            }
+          }
+        }
+        for (let prop of otherProps) {
+          newWork[prop] = work[prop]
+        }
+        this.works.push(newWork)
+    }
+  }
+
+  constructor(public translate: TranslateService, public httpService:HttpService) { }
+
+  async ngOnInit() {
     console.log(this.isFirstOnLangChangeEvent)
     this.translate.onLangChange.subscribe(this.onLangChange.bind(this))
     console.log(this.works[2].sourceCodeUrl)
+
+    this.worksData = await this.httpService.get("/projects").toPromise()
+
+    this.updateWorks("en")
   }
 
 }
