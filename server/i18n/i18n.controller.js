cd31d3df392122ea8157ad1fa81b14d1b9c27a59
Unknown <linck_192@hotmail.com>
2019-04-25 18:29:48 -0300

    i18n api delete and replace

--- server/i18n/i18n.controller.js
+++ server/i18n/i18n.controller.js
@@ -35,13 +35,35 @@ module.exports = function (router) {
   })
 
 
-  // router.route('/api/projects/:project_id')
-  //
-  //
-  // .delete(function(req, res) {
-  //   Project.remove({ _id: req.params.project_id }, function(err, project) {
-  //     if (err) res.send(err);
-  //     res.json({project: project });
-  //   })
-  // });
+  .delete(function(req, res) {
+    I18n.remove({ _lang: req.params.lang_id }, function(err, i18nDoc) {
+      if (err) res.send(err);
+      res.json({i18nDoc: i18nDoc });
+    })
+  })
+
+  .post(function(req, res) {
+
+
+    let replacementObj = req.body
+    if(!replacementObj._lang){
+      replacementObj._lang = req.params.lang_id
+    }
+
+    I18n.replaceOne({ _lang: req.params.lang_id }, replacementObj, function(err, replaceRes) {
+
+      if (err) res.send(err)
+      else {
+        if(replaceRes.nModified == 0){
+          res.json({error: `document with _lang "${req.params.lang_id}" not found.`})
+        }
+        else{
+          I18n.findOne({ _lang: replacementObj._lang }, function(err, i18nDoc) {
+            if (err) res.send(err)
+            else res.json(i18nDoc)
+          });
+        }
+      }
+    });
+  })
 }
