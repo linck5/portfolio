e513d21a8c94ada46e04ff96e1203675e4abc694
Unknown <linck_192@hotmail.com>
2019-04-25 14:11:08 -0300

    jsoneditor helloworld

--- src/app/admin-cp/content/content.component.ts
+++ src/app/admin-cp/content/content.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import JSONEditor, { JSONEditorOptions } from 'jsoneditor'
+
 
 @Component({
   selector: 'app-content',
@@ -10,6 +12,22 @@ export class ContentComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    var container = document.getElementById("jsoneditor");
+    var options:JSONEditorOptions = {
+        mode: 'tree'
+    };
+    var editor = new JSONEditor(container, options);
+
+
+    var json = {
+      "Array": [1, 2, 3],
+      "Boolean": true,
+      "Null": null,
+      "Number": 123,
+      "Object": {"a": "b", "c": "d"},
+      "String": "Hello World"
+    };
+    editor.set(json);
   }
 
 }
