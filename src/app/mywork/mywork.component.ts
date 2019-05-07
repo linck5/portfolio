6b2119c0e9130d61776bbe4272cd1c61b416a469
Unknown <linck_192@hotmail.com>
2019-05-07 17:12:09 -0300

    myWork grid layout onResize

--- src/app/mywork/mywork.component.ts
+++ src/app/mywork/mywork.component.ts
@@ -27,26 +27,16 @@ export class MyworkComponent implements OnInit, AfterViewInit {
 
 
   @ViewChildren(MyworkItemComponent) worksUL: any;
-
+  worksData:any;
   orderbyOptions:Array<OrderByOption> = [
       {id: 0, label: "", path: ["MyWork", "OrderBy", "RelevanceLabel"]},
       {id: 1, label: "", path: ["MyWork", "OrderBy", "DateLabel"]},
       {id: 2, label: "", path: ["MyWork", "OrderBy", "AlphabeticalLabel"]}
   ];
-
   selectedOrderBy:OrderByOption = this.orderbyOptions[0]
-
-
-  worksData:any;
-
-
   works:Work[] = []
-
   isFirstOnLangChangeEvent = true;
-
   workType:string = "all";
-
-
 
   private sortWorks(opt:OrderByOption){
     switch(opt.id){
@@ -75,9 +65,6 @@ export class MyworkComponent implements OnInit, AfterViewInit {
     for (let opt of this.orderbyOptions) {
         opt.label = event.translations[opt.path[0]][opt.path[1]][opt.path[2]]
     }
-
-    //this.orderbyOptions[0].label = event.translations.MyWork.OrderByRelevanceLabel
-    console.log(event)
 
     this.isFirstOnLangChangeEvent = false;
   }
@@ -116,46 +103,42 @@ export class MyworkComponent implements OnInit, AfterViewInit {
   }
 
   private updateGridLayout(){
-    console.dir(this.worksUL)
 
-    let worksULLength = this.worksUL._results.length
-    console.log("worksULLength ", worksULLength)
-
-    let g = this.resolveGridLayout(worksULLength, 4);
-
+    let ulChildren = this.worksUL._results || this.worksUL.children
+    let worksULLength = ulChildren.length
+    let g = this.resolveGridLayout(worksULLength);
     let liLackingOne = g.rowsLackingOne * (g.columns - 1);
-    console.log("liLackingOne ", liLackingOne)
 
-    let i = 0;
-    for (let resultsItem of this.worksUL._results) {
-      let li = resultsItem.myworkItem.nativeElement
+    for (let i = 0; i < worksULLength; i++) {
+      let li = ulChildren[i].myworkItem.nativeElement || ulChildren[i]
 
-        let flexStr;
+      let flexBasisPerc  = i < liLackingOne ?
+        100 / (g.columns - 1) : 100 / g.columns
 
-        if(i < liLackingOne){
-          flexStr = '1 0 ' + 100 / (g.columns - 1) + '%'
-        }
-        else {
-          flexStr = '1 0 ' + 100 / g.columns + '%'
-        }
-        console.log("i" + i + " " + flexStr)
-        li.parentElement.parentElement.style.flex = flexStr
-
-        i++;
+      li.parentElement.parentElement.style.flex = `1 0 ${flexBasisPerc}%`
     }
-
   }
 
-  private resolveGridLayout(numberOfItems, desiredNumberOfColumns){ //TODO cleanup
-    let n = numberOfItems;
-    let c = desiredNumberOfColumns;
-    while(n < c) c--;
+  private resolveGridLayout(nOfItems: number){
 
-    let rowsLackingOne = c - n % c;
-    if(c == rowsLackingOne) rowsLackingOne = 0;
+    let desiredNOfColumns: number;
+    let w = document.documentElement.clientWidth
+
+    let breakpoints = [768, 992, 1200, 1920]
+
+    desiredNOfColumns = 1;
+    for (let breakpoint of breakpoints) {
+        if(w < breakpoint) break
+        else desiredNOfColumns++
+    }
+
+    while(nOfItems < desiredNOfColumns * 2) desiredNOfColumns--
+
+    let rowsLackingOne = desiredNOfColumns - nOfItems % desiredNOfColumns
+    if(desiredNOfColumns == rowsLackingOne) rowsLackingOne = 0
 
     return {
-      columns: c,
+      columns: desiredNOfColumns,
       rowsLackingOne: rowsLackingOne,
     }
   }
@@ -163,24 +146,14 @@ export class MyworkComponent implements OnInit, AfterViewInit {
   constructor(public translate: TranslateService, public httpService:HttpService) { }
 
   ngAfterViewInit(): void {
-    this.worksUL.changes.subscribe((c) => {
-      console.log(c)
-      this.updateGridLayout()
-    })
-    //window.addEventListener('resize', this.updateGridLayout)
+    this.worksUL.changes.subscribe(c => this.updateGridLayout())
+    window.addEventListener('resize', this.updateGridLayout.bind(this))
   }
 
-
   async ngOnInit() {
-
-
-
-    console.log(this.isFirstOnLangChangeEvent)
     this.translate.onLangChange.subscribe(this.onLangChange.bind(this))
 
     this.worksData = await this.httpService.get("/projects").toPromise()
-
-
 
     this.updateWorks("en")
     this.sortWorks(this.selectedOrderBy)
