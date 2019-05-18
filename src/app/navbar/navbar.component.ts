e5be5c6011bf4b8660e4deb00bbff82da1d5b5e2
Unknown <linck_192@hotmail.com>
2019-05-17 21:50:36 -0300

    radio buttons style and checked behavior

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -13,8 +13,28 @@ export class NavbarComponent implements OnInit {
   ngOnInit() {
   }
 
-  switchLanguage(language: string) {
+  private toggleRadioCheckedById(id:string) {
+    let radioElement = document.getElementById(id);
+    (radioElement as HTMLInputElement).checked = true;
+  }
+
+  switchLanguage(event, language: string) {
     this.translate.use(language);
+
+    switch(event.srcElement.id) {
+      case "en-uncoll":
+        this.toggleRadioCheckedById("en-coll");
+        break;
+      case "pt-uncoll":
+        this.toggleRadioCheckedById("pt-coll");
+        break;
+      case "en-coll":
+        this.toggleRadioCheckedById("en-uncoll");
+        break;
+      case "pt-coll":
+        this.toggleRadioCheckedById("pt-uncoll");
+        break;
+    }
   }
 
 }
