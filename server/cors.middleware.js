b24c15a7f6e1d1b7096b2c11b041b7c2e8780c68
Unknown <linck_192@hotmail.com>
2019-06-05 13:12:46 -0300

    return 200 for OPTIONS

--- server/cors.middleware.js
+++ server/cors.middleware.js
@@ -2,5 +2,11 @@ module.exports = function (req, res, next){
   res.header('Access-Control-Allow-Origin', 'http://www.felipemuller.xyz')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE')
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200)
+  }
+
+
   next()
 }
