3475cdaad8eb4bfb089f1f0bc1e6d1dbcdd8ecad
Unknown <linck_192@hotmail.com>
2019-06-05 13:22:19 -0300

    fix: was not actually sending anything on OPTIONS method request

--- server/cors.middleware.js
+++ server/cors.middleware.js
@@ -4,7 +4,7 @@ module.exports = function (req, res, next){
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, DELETE')
 
   if (req.method === 'OPTIONS') {
-    return res.status(200)
+    return res.send(200)
   }
 
 
