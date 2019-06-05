c6eaed216c6c88a627ad9c6960b75d8143ff4953
Unknown <linck_192@hotmail.com>
2019-06-05 12:43:00 -0300

    fix: wrong protocol on allow origin

--- server/cors.middleware.js
+++ server/cors.middleware.js
@@ -1,5 +1,5 @@
 module.exports = function (req, res, next){
-  res.header('Access-Control-Allow-Origin', 'https://www.felipemuller.xyz')
+  res.header('Access-Control-Allow-Origin', 'http://www.felipemuller.xyz')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   res.header('Access-Control-Allow-Methods', 'GET, POST, DELETE')
   next()
